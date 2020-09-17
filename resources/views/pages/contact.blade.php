b31282192f0a348551f24e869b1dd2b38f170729
Laramiadell <71432940+Laramiadell@users.noreply.github.com>
2020-09-17 20:08:38 +0800

    descriptive comment for code changes

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prelim Project | Contact</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+</head>
+<body>
+    <h1>CONTACT</h1>
+<a href="{{url('/')}}">Home</a>| Contact |<a href="{{url('/index')}}">Profile</a>
+</body>
+</html>
